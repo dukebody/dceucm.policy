98edb4afae66a577ac5e2ba3a2bb480999eafdf9
Israel Saeta <dukebody at gmail dot com>
2009-02-22 17:13:09 +0100

    set default language to spanish

--- dceucm/policy/tests/test_setup.py
+++ dceucm/policy/tests/test_setup.py
@@ -7,6 +7,9 @@ class TestSetup(TestCase):
         self.assertEquals("Delegación Central de Estudiantes UCM", self.portal.getProperty('title'))
     def test_portal_description(self):
         self.assertEquals("Sitio web de la Delegación Central de Estudiantes de la Universidad Complutense de Madrid", self.portal.getProperty('description'))
+    def test_portal_language(self):
+        self.assertEquals("es", 
+            self.portal.portal_properties.site_properties.getProperty('default_language'))
 
 def test_suite():
     suite = unittest.TestSuite()
